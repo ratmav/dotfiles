9fc50955955ad8fe7767b72ba2def11c6268ecee
Chris Cuming <ccuming@gmail.com>
2016-03-04 00:56:42 -0500

    cleaning up old git dir, removing bashrc - not much in that, really.

--- bash/scripts/remove-all-gems.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Uninstalls all gems for current Ruby version, re-installs Bundler.
-# NOTE: Requires rbenv.
-
-for i in `gem list --no-versions`; do gem uninstall -aIx $i; done && gem install bundler && rbenv rehash
