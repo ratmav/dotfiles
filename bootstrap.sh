26775de8d169c4414f6898ff41045429cd8d5058
Chris Cuming <ccuming@gmail.com>
2016-03-26 21:53:11 -0400

    append to existing profile; update readme so i don't forget a recursive clone next time, trim down bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-echo "copying dotfiles to home directory..."
-cp -R ./ ~/
+echo "copying tmux config..."
+cp .tmux.conf ~/
+echo "..done"
+
+echo "copying vim config and plugins..."
+cp .vimrc ~/
+cp -R .vim ~/
 echo "..done"
 
 echo "installing fonts..."
-fc-cache -f -v
+cp -R .fonts ~/
+fc-cache -f -v >> /dev/null &>/dev/null # STFU.
+echo "..done"
+
+echo "installing scripts..."
+printf "\nexport PATH=\"\$PATH:\$HOME/Scripts\"" >> ~/.profile
+cp -R Scripts ~/
 echo "..done"
 
 echo "configuring global gitignore..."
-git config --global core.excludesfile '~/.gitignore'
+cp .gitignore_global ~/
+git config --global core.excludesfile '~/.gitignore_global'
 echo "..done"
