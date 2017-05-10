fb589ba9d98b35b09592ff1bc01cb45aad2dcfa1
Chris Cuming <ratmav@protonmail.com>
2017-05-09 22:37:08 -0400

    Bootstrap spacemacs and friends.

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 dotfiles
 ========
 
-personal configuration and documentation collection project for general purpose use.
-
 # Use
 
 ## Install
 
+### *nix
+
 ```bash
-$ git clone --recursive https://github.com/ratmav/dotfiles.git
+$ git clone  https://github.com/ratmav/dotfiles.git
 $ cd dotfiles && ./bootstrap.sh && source ~/.bash_profile
 ```
 
 ## Update
 
+### *nix
+
 ```bash
 $ cd dotfiles
-$ git pull --recurse-submodules
 $ ./bootstrap.sh && source ~/.bash_profile
 ```
---
+++ bash/mac_os.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+source ./bash/python.sh
+source ./bash/node.sh
+
+copy_fonts_mac() {
+  echo "...copying fonts on mac..."
+  cp fonts/* ~/Library/Fonts/
+  echo "......copied fonts"
+}
+
+install_git() {
+  echo "...checking git install..."
+  if type git > /dev/null 2>&1; then
+    echo "......git already installed"
+  else
+    brew install git
+    echo "......installed git"
+  fi
+}
+
+install_homebrew() {
+  echo "...checking homebrew install..."
+  if type brew > /dev/null 2>&1; then
+    echo "......homebrew already installed"
+  else
+    URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
+    /usr/bin/ruby -e "$(curl -fsSL $URL)"
+    echo "......installed homebrew"
+  fi
+}
+
+install_spacemacs() {
+  echo "...checking spacemacs install..."
+  if brew list | grep emacs > /dev/null 2>&1; then
+    echo "......spacemacs already installed"
+  else
+    brew tap d12frosted/emacs-plus
+    brew install emacs-plus --HEAD --with-natural-title-bars
+    brew linkapps emacs-plus
+    rm -rf $HOME/.emacs.d
+    git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+    echo "......installed spacemacs"
+  fi
+}
+
+
+bootstrap_mac_os() {
+  copy_fonts_mac
+  install_homebrew
+  install_git
+  install_spacemacs
+  bootstrap_node
+  bootstrap_python
+}
