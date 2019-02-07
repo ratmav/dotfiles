1b265d56cb8df54de95d4c712441490af3f2a85e
Chris Cuming <ratmav@protonmail.com>
2019-02-07 13:29:17 -0500

    removing rbenv and grip.

--- bash/mac_os.sh
+++ bash/mac_os.sh
@@ -17,7 +17,7 @@ install_homebrew() {
 
 homebrew() {
   PACKAGES=("git" "python" "tmux" "reattach-to-user-namespace" "neovim" "pyenv"
-            "pyenv-virtualenv" "rbenv" "grip")
+            "pyenv-virtualenv")
   for package in "${PACKAGES[@]}"; do
     echo "...checking $package install..."
     if brew list | grep $package > /dev/null 2>&1; then
