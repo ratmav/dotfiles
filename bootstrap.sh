539c805fc3db73dbb4abe2c6735cd60c86745b7d
Chris Cuming <ratmav@protonmail.com>
2019-10-11 11:15:34 -0400

    return of powerline and some tmux snazziness

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,14 +52,23 @@ operating_system() {
   fi
 }
 
+powerline_fonts() {
+  git clone https://github.com/powerline/fonts.git --depth=1
+  cd fonts
+  ./install.sh
+  cd ..
+  rm -rf fonts
+}
+
 main() {
-  operating_system
-  tpm
-  home_symlinks
-  nvim_init
-  vim_plug
-  configure_gitignore
-  configure_git_editor
+  #operating_system
+  #tpm
+  #home_symlinks
+  #nvim_init
+  #vim_plug
+  #configure_gitignore
+  #configure_git_editor
+  powerline_fonts
 }
 
 main "$@"
