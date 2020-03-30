c34c31c7f9bc4c09b00aa08d6c6a3c34e76dada8
Chris Cuming <ratmav@protonmail.com>
2020-03-30 14:59:48 -0400

    vscodium...mehhhhh

--- bash/mac_os.sh
+++ bash/mac_os.sh
@@ -27,7 +27,7 @@ brew_packages() {
 }
 
 cask_packages() {
-  PACKAGES=("firefox-developer-edition" "vscodium")
+  PACKAGES=("firefox-developer-edition")
   for package in "${PACKAGES[@]}"; do
     echo "...checking $package install"
     if brew cask list | grep $package > /dev/null 2>&1; then
