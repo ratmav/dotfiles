b7e1c46fd691c20b99c47ea268f3eb7154660414
Chris Cuming <ratmav@protonmail.com>
2020-04-06 18:20:30 -0400

    adding pandoc to macos.

--- bash/mac_os.sh
+++ bash/mac_os.sh
@@ -14,7 +14,7 @@ homebrew() {
 
 brew_packages() {
   PACKAGES=("bash-completion" "git" "neovim" "reattach-to-user-namespace" "tmux"
-    "clamav" "bash" "shellcheck" "tree" "grep")
+    "clamav" "bash" "shellcheck" "tree" "grep" "pandoc")
   for package in "${PACKAGES[@]}"; do
     echo "...checking $package install"
     if brew list | grep $package > /dev/null 2>&1; then
