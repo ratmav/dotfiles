b97d92ec981f9237babe6480a2ad7422bf787c31
Chris Cuming <ratmav@protonmail.com>
2021-02-01 00:05:02 -0500

    auto-install vim plugins.

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,6 +35,9 @@ vim_plug() {
   rm -rf $HOME/.local/share/nvim
   URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs -s $URL
+
+  echo "...installing plugins"
+  nvim +PlugInstall +qall
 }
 
 operating_system() {
