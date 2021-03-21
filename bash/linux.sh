30d100a418b2d446eebb7ac99edec0736ba30be9
Chris Cuming <ratmav@protonmail.com>
2021-03-20 23:56:45 -0400

    ehhhhh, ansible isn't going to help much with this.

--- bash/linux.sh
+++ bash/linux.sh
@@ -2,28 +2,22 @@
 
 # TODO:
 # set up a vargrant image and do this to keep the metal clean.
-# * install docker
-# * run ansible in a container
-# * configure ansible to use files in the repo
-# * playbooks/tasks
-#   * uninstall packages you don't need
-#     * kate
-#     * yaquake
-#     * konversation
-#   * install packages you need
-#     * brave
-#     * tresorit
-#     * oni (markdown preview? probably a vscode extension)
-#     * vagrant
-#     * docker (just in case)
-#     * asdf
-#     * shellcheck (also set up a github action on this repo)
-#     * signal
-#     * steam/proton/etc.
-#     * battle.net
-#     * uhk agent
-# * uninstall nvim (manually)
-# * break symlinks?,
+# * uninstall packages you don't need
+#   * kate
+#   * yaquake
+#   * konversation
+# * install packages you need
+#   * brave
+#   * tresorit
+#   * vagrant
+#   * docker (just in case)
+#   * asdf
+#   * shellcheck (also set up a github action on this repo)
+#   * signal
+#   * steam/proton/etc.
+#   * battle.net
+#   * uhk agent
+#   * oni (after getting the rest; markdown preview? probably a vscode extension)
 
 install_docker() {
 
