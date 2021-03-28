04f0b4f1201fc79305a24d07d8df95660dfa3c01
Chris Cuming <ratmav@protonmail.com>
2021-03-27 23:04:45 -0400

    wooo

--- bash/debian.sh
+++ bash/debian.sh
@@ -3,20 +3,14 @@
 # TODO:
 # set up a vargrant image and do this to keep the metal clean.
 # * install packages you need
-#   * brave
-#   * tresorit
-#   * vagrant
-#   * docker (just in case)
-#   * shellcheck (also set up a github action on this repo)
-#   * signal
-#   * steam/proton/etc.
-#   * battle.net
-#   * uhk agent
-#   * oni (after getting the rest; markdown preview? probably a vscode extension)
+#   * signal: https://signal.org/download/#
+#   * vagrant: https://www.hashicorp.com/blog/announcing-the-hashicorp-linux-repository
+#   * docker: https://docs.docker.com/engine/install/debian/
+#   * virtualbox: https://tecadmin.net/install-virtualbox-on-debian-10-buster/
 
 debian_brave() {
-  PACKAGES=("apt-transport-https" "curl" "gnugpg")
-  for package in "${PACKAGES[@]}"; do
+  packages=("apt-transport-https" "curl" "gnupg")
+  for package in "${packages[@]}"; do
     if dpkg -l | grep -w $package > /dev/null 2>&1; then
       msg "${OK}debian_brave: $package already installed."
     else
@@ -24,39 +18,19 @@ debian_brave() {
       msg "${OK}debian_brave: installed $package."
     fi
   done
-}
 
-debian_fuse() {
-  if dpkg -l | grep -w fuse > /dev/null 2>&1; then
-    msg "${OK}debian_fuse: fuse already installed."
-  else
-    quiet "sudo apt-get install -y fuse"
-    quiet "sudo modprobe fuse"
+  sudo rm -f /etc/apt/sources.list.d/brave-browser-release.list
+  sudo bash -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"\
+    >| /etc/apt/sources.list.d/brave-browser-release.list'
+  msg "${OK}debian_brave_repo: configured apt."
 
-    quiet "sudo groupadd fuse"
-    user="$(whoami)"
-    quiet "sudo usermod -a -G fuse $user"
+  curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc\
+    | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+  msg "${OK}debian_brave: gpg key added."
 
-    msg "${WARN}debian_fuse: installed fuse; relogin likely required."
-  fi
-}
-
-debian_oni_dependencies() {
-  PACKAGES=("apt-utils" "build-essential" "libtool" "gettext" "nasm" "libacl1-dev"
-    "libncurses-dev" "libglu1-mesa-dev" "libxxf86vm-dev" "libxkbfile-dev"
-    "git-core" "curl" "libpng-dev" "libbz2-dev" "m4" "xorg-dev"
-    "libharfbuzz-dev" "libgtk-3-dev" "libfontconfig1-dev" "clang"
-    "software-properties-common" "dialog" "wget" "gpg")
-  for package in "${PACKAGES[@]}"; do
-    if dpkg -l | grep -w $package > /dev/null 2>&1; then
-      msg "${OK}debian_oni_dependencies: $package already installed."
-    else
-      quiet "sudo apt-get install -y $package"
-      msg "${OK}debian_oni_dependencies: installed $package."
-    fi
-  done
-
-  debian_fuse
+  quiet "sudo apt-get update"
+  quiet "sudo apt-get install brave-browser -y"
+  msg "${OK}debian_brave: installed brave."
 }
 
 debian_update() {
@@ -65,8 +39,23 @@ debian_update() {
   msg "${OK}debian_update: system updated."
 }
 
+debian_dependencies() {
+  packages=("build-essential" "shellcheck" "gnupg" "clamav" "git")
+  for package in "${packages[@]}"; do
+    if dpkg -l | grep -w $package > /dev/null 2>&1; then
+      msg "${OK}debian_dependencies: $package already installed."
+    else
+      quiet "sudo apt-get install -y $package"
+      msg "${OK}debian_dependencies: installed $package."
+    fi
+  done
+}
+
 main_debian() {
   debian_update
-  debian_oni_dependencies
+  debian_dependencies
   debian_brave
+
+  msg "${WARN}main_debian: docker-compose needs manual installation from https://github.com/docker/compose/releases/latest"
+  msg "${WARN}main_debian: uhk agent needs manual installation from https://github.com/UltimateHackingKeyboard/agent/releases/latest"
 }
