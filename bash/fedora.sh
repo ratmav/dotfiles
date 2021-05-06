d4c3aadf3217847a2c1c252e580480d7f03fb934
Chris Cuming <ratmav@protonmail.com>
2021-05-06 18:14:35 -0400

    yyyeeeaaaahhh let's just install gui apps manually. everybody has their own package management...ideas.

--- bash/fedora.sh
+++ bash/fedora.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env/bash
 
-fedora_brave() {
-  if grep -q "Fedora" /etc/system-release; then
-    fedora_update
-    if dnf list --installed | grep -w brave-browser > /dev/null 2>&1; then
-      msg "${WARN}fedora_brave: brave already installed."
-    else
-      quiet "sudo dnf install -y dnf-plugins-core"
-      quiet "sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/"
-      quiet "sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc"
-      quiet "sudo dnf install -y brave-browser"
-      msg "${OK}fedora_brave: installed brave."
-    fi
-  else
-    die "fedora_brave: unsupported operating system."
-  fi
-}
-
 fedora_dependencies() {
   if grep -q "Fedora" /etc/system-release; then
     fedora_update
-    packages=("neovim" "powerline-fonts" "pandoc")
+    packages=("neovim" "pandoc")
     for package in "${packages[@]}"; do
       if dnf list --installed | grep -w $package > /dev/null 2>&1; then
         msg "${WARN}fedora_dependencies: $package already installed."
@@ -46,7 +29,6 @@ fedora_update() {
 main_fedora() {
   if grep -q "Fedora" /etc/system-release; then
     fedora_dependencies
-    fedora_brave
   else
     die "main_fedora: unsupported operating system."
   fi
