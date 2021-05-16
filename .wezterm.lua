afffe0f7846a0c9a0b10138600bb785463a6e7e3
Chris Cuming <ratmav@protonmail.com>
2021-05-16 00:12:35 -0400

    some term management keybindings.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,36 @@
 local wezterm = require 'wezterm';
+
 return {
-  disable_default_key_bindings = true,
+  -- display
   color_scheme = "Gruvbox Dark",
   font = wezterm.font(
     "Source Code Pro for Powerline",
-    {weight="Medium"}
+    { weight="Medium" }
   ),
   font_size = 16,
   warn_about_missing_glyphs = false,
+
+  -- behavior
+  disable_default_key_bindings = true,
+  leader = { key="Control", mods="SHIFT" },
+  keys = {
+    {
+      key="n",
+      --mods="CTRL|SHIFT",
+      mods="LEADER",
+      action=wezterm.action{SpawnTab="DefaultDomain"},
+    },
+    {
+      key="h",
+      --mods="CTRL|SHIFT",
+      mods="LEADER",
+      action=wezterm.action{ActivateTabRelative=-1},
+    },
+    {
+      key="l",
+      --mods="CTRL|SHIFT",
+      mods="LEADER",
+      action=wezterm.action{ActivateTabRelative=1},
+    },
+  },
 }
