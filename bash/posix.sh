915c065255430710c833e67f2b4ace82a528a2b6
Chris Cuming <ratmav@protonmail.com>
2021-06-02 22:57:18 -0400

    posix funcname.

--- bash/posix.sh
+++ bash/posix.sh
@@ -2,40 +2,40 @@
 
 posix_asdf() {
   if [ -f "$HOME/.asdf/asdf.sh" ]; then
-    msg "${WARN}posix_asdf: asdf installed."
+    msg "${WARN}${FUNCNAME[0]}: asdf installed."
   else
     rm -rf $HOME/.asdf
     git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf > /dev/null 2>&1
     msg "${OK}posix_asdf: asdf installed."
-    msg "${WARN}posix_asdf: sourcing ~/.bashrc may be required."
+    msg "${WARN}${FUNCNAME[0]}: sourcing ~/.bashrc may be required."
   fi
 }
 
 posix_git() {
   git config --global core.excludesfile "$HOME/.gitignore_global"
-  msg "${OK}posix_git: configured global gitignore."
+  msg "${OK}${FUNCNAME[0]}: configured global gitignore."
 
   # TODO: maaaybe should be in a git config you can symlink to (also gpg config).
   git config --global core.editor "$(which nvim)"
-  msg "${OK}posix_git: configured git editor."
+  msg "${OK}${FUNCNAME[0]}: configured git editor."
 
   git config --global url."git@gitlab.com:".insteadOf "https://gitlab.com/"
-  msg "${OK}posix_git: configured git to connect to gitlab over ssh."
+  msg "${OK}${FUNCNAME[0]}: configured git to connect to gitlab over ssh."
 }
 
 posix_nvim() {
   rm -rf $HOME/.config/nvim
   mkdir -p $HOME/.config/nvim
   ln -s $PWD/init.vim $HOME/.config/nvim/init.vim
-  msg "${OK}posix_nvim: symlinked nvim config."
+  msg "${OK}${FUNCNAME[0]}: symlinked nvim config."
 
   rm -rf $HOME/.local/share/nvim
   URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs -s $URL
-  msg "${OK}posix_nvim: installed vim-plug."
+  msg "${OK}${FUNCNAME[0]}: installed vim-plug."
 
   nvim +PlugInstall +qall
-  msg "${OK}posix_nvim: installed neovim plugins."
+  msg "${OK}${FUNCNAME[0]}: installed neovim plugins."
 }
 
 posix_symlinks() {
@@ -43,7 +43,7 @@ posix_symlinks() {
   for link in "${LINKS[@]}"; do
     rm -rf $HOME/$link
     ln -s $PWD/$link $HOME/$link
-    msg "${OK}posix_symlinks: symlinked $link"
+    msg "${OK}${FUNCNAME[0]}: symlinked $link"
   done
 }
 
