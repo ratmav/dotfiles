3dccb035980298fff885b3191ca3470cb8ba5bf2
Chris Cuming <chris_cuming@mckinsey.com>
2021-07-02 00:54:33 -0400

    second pass wez launch menu.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,61 +1,6 @@
 local wezterm = require 'wezterm';
 
-function os_shell()
-  -- typical install local of git bash on windows.
-  local git_bash_path = "C:\\Program Files\\Git\\bin\\bash.exe"
-
-  -- package.config:sub(1,1) returns '/' for *nix and '\' for *windows.
-  if package.config:sub(1,1) ~= '/' then
-    -- if git bash is present, default to it.
-    if io.open(git_bash_path) ~= nil then
-      return {git_bash_path}
-    -- if git bash is not present, fallback to powershell.
-    else
-      return {"powershell", "-NoLogo"}
-    end
-  -- on *nix, open a bash login shell.
-  else
-    return {"bash", "-l"}
-  end
-end
-
-function os_launch()
-  -- typical install local of git bash on windows.
-  local git_bash_path = "C:\\Program Files\\Git\\bin\\bash.exe"
-  local launch_menu = {}
-
-  -- package.config:sub(1,1) returns '/' for *nix and '\' for *windows.
-  if package.config:sub(1,1) ~= '/' then
-    -- if we're on windows, add powershell.
-    table.insert(launch_menu, {
-      label = "powershell",
-      args = {"powershell", "-NoLogo"},
-    })
-    -- if git bash is installed, add git bash.
-    if io.open(git_bash_path) ~= nil then
-    table.insert(launch_menu, {
-      label = "git bash",
-      args = {git_bash_path},
-    })      
-    end
-  -- on *nix, add bash.
-  else
-    table.insert(launch_menu, {
-      label = "WEZbash",
-      args = {"bash", "-l"},
-    })
-  end
-  
-  return launch_menu
-end
-
-return {
-  -- set the default shell based on the os.
-  default_prog = os_shell(),
-
-  -- set the launch menu based on the os.
-  launch_menu = os_launch(),
-
+config = {
   -- display
   color_scheme = "Gruvbox Dark",
   font_size = 16,
@@ -97,3 +42,29 @@ return {
     },
   },
 }
+
+-- package.config:sub(1,1) returns '/' for *nix and '\' for *windows.
+if package.config:sub(1,1) ~= '/' then
+    -- typical install local of git bash on windows.
+    local git_bash_path = "C:\\Program Files\\Git\\bin\\bash.exe"
+    local launch_menu = {
+      label = "powershell",
+      args = {"powershell", "-NoLogo"},
+    }
+
+    -- if git bash is present, default to it.
+    if io.open(git_bash_path) ~= nil then
+      config.insert(default_prog, {git_bash_path})
+      table.insert(launch_menu, {
+        label = "git bash",
+        args = {git_bash_path}, 
+      })
+    -- if git bash is not present, fallback to powershell.
+    else
+      config.insert(default_prog, {"powershell", "-NoLogo"})
+    end
+
+    config.insert(launch_menu, launch_menu)
+end
+
+return config
