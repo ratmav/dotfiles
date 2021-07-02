98a5d3501fdd370fb6b25ad2ecbbe2050ff1375e
Chris Cuming <ratmav@protonmail.com>
2021-07-02 01:13:06 -0400

    third pass at wez on windows; confirmed working on macos and windows.

--- .wezterm.lua
+++ .wezterm.lua
@@ -45,26 +45,26 @@ config = {
 
 -- package.config:sub(1,1) returns '/' for *nix and '\' for *windows.
 if package.config:sub(1,1) ~= '/' then
-    -- typical install local of git bash on windows.
-    local git_bash_path = "C:\\Program Files\\Git\\bin\\bash.exe"
-    local launch_menu = {
-      label = "powershell",
-      args = {"powershell", "-NoLogo"},
-    }
+  -- typical install local of git bash on windows.
+  local git_bash_path = "C:\\Program Files\\Git\\bin\\bash.exe"
+  local launch_menu = {{
+    label = "powershell",
+    args = {"powershell", "-NoLogo"},
+  }}
 
-    -- if git bash is present, default to it.
-    if io.open(git_bash_path) ~= nil then
-      config.insert(default_prog, {git_bash_path})
-      table.insert(launch_menu, {
-        label = "git bash",
-        args = {git_bash_path}, 
-      })
-    -- if git bash is not present, fallback to powershell.
-    else
-      config.insert(default_prog, {"powershell", "-NoLogo"})
-    end
+  -- if git bash is present, default to it and add to the launch menu.
+  if io.open(git_bash_path) ~= nil then
+    config.default_prog = {git_bash_path}
+    table.insert(launch_menu, {
+      label = "git bash",
+      args = {git_bash_path},
+    })
+  -- if git bash is not present, fallback to powershell.
+  else
+    config.default_prog = {"powershell", "-NoLogo"}
+  end
 
-    config.insert(launch_menu, launch_menu)
+  config.launch_menu = launch_menu
 end
 
 return config
