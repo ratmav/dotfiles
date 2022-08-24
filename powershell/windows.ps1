c71ba86b64b0d3cacaa2cd2b375aebe9f30fa722
Chris Cuming <ratmav@protonmail.com>
2022-08-23 23:59:40 -0400

    cleaner logging.

--- powershell/windows.ps1
+++ powershell/windows.ps1
@@ -9,12 +9,12 @@ function configure_git {
     } else {
       die "neovim not installed"
     }
+
     git config core.eol lf
     git config core.autocrlf input
     git config --global core.eol lf
     git config --global core.autocrlf input
-
-    info "configure git to use lf, not crlf, line endings"
+    info "configured git to use lf, not crlf, line endings"
   } else {
     die "git not installed"
   }
@@ -62,6 +62,7 @@ function install_psscriptanalyzer {
     -FilePath "powershell" `
     -Verb runAs `
     -ArgumentList "Install-Module -Name PSScriptAnalyzer -Force"
+  info "(re)installed psscriptanalyzer"
 }
 
 function winget_packages {
