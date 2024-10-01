e34ceaf1a6cccf94fb06cb840f3f2ee36db6aa7d
Chris Watkins <chris@sixfeetup.com>
2024-10-01 16:01:16 -0400

    add cosign install to macos_brew_packages for installer verification with asdf.

--- bash/macos.sh
+++ bash/macos.sh
@@ -19,7 +19,7 @@ macos_brew_packages() {
     # librsvg and python are used with pandoc.
     PACKAGES=("shellcheck" "coreutils" "bash-completion" "neovim"
       "reattach-to-user-namespace" "bash" "grep" "pandoc" "librsvg" "python"
-      "gpg" "git")
+      "gpg" "git" "cosign")
     for package in "${PACKAGES[@]}"; do
       if eval "$(/opt/homebrew/bin/brew shellenv)" && brew list | grep $package > /dev/null 2>&1; then
         msg "${WARN}${FUNCNAME[0]}: $package already installed."
