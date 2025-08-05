b444e5cd5f7ee2fb10c407e3632990fbe83ffc78
Chris Watkins <chris@sixfeetup.com>
2025-08-05 16:54:41 -0400

    adding csp sdk's.

--- nix/base.nix
+++ nix/base.nix
@@ -1,13 +1,20 @@
 let
   stable = import <nixpkgs> { };
   unstable = import <nixpkgs-unstable> { };
+  gdk = stable.google-cloud-sdk.withExtraComponents( with stable.google-cloud-sdk.components; [
+    gke-gcloud-auth-plugin
+  ]);
 in
 (with stable; [
+  awscli2
+  azure-cli
   curl
   direnv
+  gh
   git
   gnugrep
   opentofu
   gnused
 ])
+++ [ gdk ]
 ++ (with unstable; [ uv ])
