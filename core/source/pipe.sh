4b9a79b2e046b2acd54409f3d1b9392544a1ca01
Chris Watkins <ratmav@protonmail.com>
2026-03-01 17:09:05 -0500

    wip

--- core/source/pipe.sh
+++ core/source/pipe.sh
@@ -21,37 +21,21 @@ ish_pipe_connect() {
 }
 
 ish_pipe_compose() {
-  local -a cmds=("$@") pids=()
-  local tmpdir i last_status
+  local -a cmds=("$@")
 
   [[ ${#cmds[@]} -lt 2 ]] && _pipe_error "at least two commands required"
 
   # FIFOs, not eval or case dispatch. eval does not preserve PIPESTATUS.
   # Case dispatch preserves PIPESTATUS but caps stage count artificially.
   # Recursive approach loses middle-stage status to subshell boundaries.
-  tmpdir=$(mktemp -d) || _pipe_error "failed to create temp directory"
+  local tmpdir
+  tmpdir=$(_pipe_create_fifos "${#cmds[@]}")
 
-  for ((i = 0; i < ${#cmds[@]} - 1; i++)); do
-    mkfifo "${tmpdir}/pipe_${i}" || { rm -rf "$tmpdir"; _pipe_error "failed to create fifo"; }
-  done
+  local -a pids=()
+  _pipe_run_stages cmds pids "$tmpdir"
 
-  "${cmds[0]}" > "${tmpdir}/pipe_0" &
-  pids+=($!)
-
-  for ((i = 1; i < ${#cmds[@]} - 1; i++)); do
-    "${cmds[$i]}" < "${tmpdir}/pipe_$((i - 1))" > "${tmpdir}/pipe_${i}" &
-    pids+=($!)
-  done
-
-  "${cmds[-1]}" < "${tmpdir}/pipe_$((${#cmds[@]} - 2))"
-  last_status=$?
-
-  ISH_PIPE_STATUS=()
-  for pid in "${pids[@]}"; do
-    wait "$pid"
-    ISH_PIPE_STATUS+=($?)
-  done
-  ISH_PIPE_STATUS+=("$last_status")
+  local last_status=$?
+  _pipe_collect_status pids "$last_status"
 
   rm -rf "$tmpdir"
 }
@@ -83,6 +67,48 @@ ish_pipe_require_success() {
 }
 
 # Private functions
+
+_pipe_create_fifos() {
+  local stage_count="$1"
+  local tmpdir i
+
+  tmpdir=$(mktemp -d) || _pipe_error "failed to create temp directory"
+
+  for ((i = 0; i < stage_count - 1; i++)); do
+    mkfifo "${tmpdir}/pipe_${i}" || { rm -rf "$tmpdir"; _pipe_error "failed to create fifo"; }
+  done
+
+  printf '%s' "$tmpdir"
+}
+
+_pipe_run_stages() {
+  local -n _cmds=$1 _pids=$2
+  local tmpdir="$3"
+  local i
+
+  "${_cmds[0]}" > "${tmpdir}/pipe_0" &
+  _pids+=($!)
+
+  for ((i = 1; i < ${#_cmds[@]} - 1; i++)); do
+    "${_cmds[$i]}" < "${tmpdir}/pipe_$((i - 1))" > "${tmpdir}/pipe_${i}" &
+    _pids+=($!)
+  done
+
+  "${_cmds[-1]}" < "${tmpdir}/pipe_$((${#_cmds[@]} - 2))"
+}
+
+_pipe_collect_status() {
+  local -n _pids_ref=$1
+  local last_status="$2"
+  local pid
+
+  ISH_PIPE_STATUS=()
+  for pid in "${_pids_ref[@]}"; do
+    wait "$pid"
+    ISH_PIPE_STATUS+=($?)
+  done
+  ISH_PIPE_STATUS+=("$last_status")
+}
 
 _pipe_error() {
   printf '%s\n' "${ISH_COLOR_RED}ish_pipe: ${1}${ISH_COLOR_CLEAR}" >&2
